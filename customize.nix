dba5949e475a2da9747e9632dc306c2aaa56a86d
Jonas Juselius <jonas.juselius@itpartner.no>
2020-11-25 21:04:49 +0100

    Update customize with hpc options

--- customize.nix
+++ customize.nix
@@ -1,15 +1,5 @@
 { pkgs, config, ...}:
 {
-
-  networking = {
-    domain = "itpartner.intern";
-    search = [ "itpartner.intern" "itpartner.no" ];
-  };
-
-  users.extraUsers.root.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKiAS30ZO+wgfAqDE9Y7VhRunn2QszPHA5voUwo+fGOf jonas-3"
-  ];
-
   customize = {
     desktop.enable = false;
 
@@ -29,10 +19,33 @@
 
     lan.enable = false;
 
-    dnsmasq.enable = true;
-    dnsmasq.extraConfig = ''
+    hpc.slurm = {
+      server = true;
+      controlMachine = "regnekraft";
+      nodeName = [
+        "DEFAULT CPUs=1 RealMemory=196000 TmpDisk=250000 State=UNKNOWN"
+        "c0-[1-8] NodeAddr=c0-[1-8]"
+      ];
+      partitionName = [
+        "run Nodes=c0-[1-8] Default=YES MaxTime=INFINITE State=UP"
+      ];
+      storagePass  = "olj2jememd";
+    };
+  };
+
+  networking = {
+    hostName = "regnekraft";
+    domain = "itpartner.intern";
+    search = [ "itpartner.intern" "itpartner.no" ];
+  };
+
+  services.dnsmasq.enable = true;
+  services.dnsmasq.extraConfig = ''
       address=/.cluster.local/10.101.0.1
     '';
-  };
+
+  users.extraUsers.root.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKiAS30ZO+wgfAqDE9Y7VhRunn2QszPHA5voUwo+fGOf jonas-3"
+  ];
 }
 
