59722745717174fafe5ed5bfea5653fca1c74909
Jonas Juselius <jonas.juselius@itpartner.no>
2020-12-07 14:52:06 +0100

    Cleanup defaults

--- hosts.nix
+++ hosts.nix
@@ -4,20 +4,5 @@ let
 in
 {
   networking.extraHosts = ''
-    10.1.62.2 stokes stokes.hpc.local stokes.regnekraft.io
-
-    10.1.61.200 mds0-0 mds0-0.hpc.local
-
-    10.1.61.201 c0-0 c0-0.hpc.local stokes-intern.hpc.local
-    10.1.61.202 c0-1 c0-1.hpc.local
-    10.1.61.203 c0-2 c0-2.hpc.local
-    10.1.61.204 c0-3 c0-3.hpc.local
-    10.1.61.205 c0-4 c0-4.hpc.local
-    10.1.61.206 c0-5 c0-5.hpc.local
-    10.1.61.207 c0-6 c0-6.hpc.local
-    10.1.61.208 c0-7 c0-7.hpc.local
-    10.1.61.209 c0-8 c0-8.hpc.local
-
-    10.1.2.74 yoneda yoneda.itpartner.intern
   '';
 }
--- kernel.nix
+++ kernel.nix
@@ -1,51 +1,12 @@
 {pkgs, stdenv, fetchurl, config, kernel ? null, ...}:
 let
   kernel = if kernel == null then config.system.build.kernel else kernel;
-  e1000e =
-    # assert stdenv.lib.versionOlder kernel.version "4.10";
-    stdenv.mkDerivation rec {
-      name = "e1000e-${version}-${kernel.version}";
-      version = "3.6.0";
-
-      src = fetchurl {
-        url = "mirror://sourceforge/e1000/e1000e-${version}.tar.gz";
-        sha256 = "0878j3rxlxckf71g3sv1rwf9077pq0almgp46q5vmyinsbb0zna1";
-      };
-
-      hardeningDisable = [ "pic" ];
-
-      configurePhase = ''
-        cd src
-        kernel_version=${kernel.modDirVersion}
-        sed -i -e 's|/lib/modules|${kernel.dev}/lib/modules|' Makefile
-        sed -i -e 's|/lib/modules|${kernel.dev}/lib/modules|' common.mk
-        export makeFlags="BUILD_KERNEL=$kernel_version"
-      '';
-
-      installPhase = ''
-        install -v -D -m 644 e1000e.ko "$out/lib/modules/$kernel_version/kernel/drivers/net/e1000e/e1000e.ko"
-      '';
-
-      dontStrip = true;
-
-      enableParallelBuilding = true;
-
-      meta = {
-        description = "Linux kernel drivers for Intel Ethernet adapters and LOMs (LAN On Motherboard)";
-        homepage = http://e1000.sf.net/;
-        license = stdenv.lib.licenses.gpl2;
-      };
-    };
-
-  e1000Package = self: super: {
-    linuxPackages = super.linuxPackages // { inherit e1000e; };
-  };
 in
   {
-    # nixpkgs.overlays = [ e1000Package ];
+    nixpkgs.overlays = [];
 
     boot = {
-      # extraModulePackages = [ pkgs.linuxPackages.e1000e ];
-      # kernelPackages = pkgs.linuxPackages_5_2;
+      extraModulePackages = [];
+      kernelPackages = kernel;
     };
   }
