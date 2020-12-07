de94826e2158a925931beeb24a43f6c90d84ebb9
Jonas Juselius <jonas.juselius@itpartner.no>
2020-12-07 14:37:32 +0100

    Cleanup default configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,8 +1,7 @@
 { pkgs, config, ...}:
 {
   imports = [
-    ./modules
-    ./cachix.nix
+    ./features
     ./users.nix
     ./hosts.nix
     ./kernel.nix
@@ -11,75 +10,33 @@
   ];
 
   networking = {
-    hostName = "stokes";
-    domain = "regnekraft.io";
-    search = [ "regnekraft.io" "itpartner.intern" "itpartner.no" ];
+    hostName = "nixos";
+    domain = "local";
+    search = [ "local" ];
   };
 
-  feature = {
+  features = {
     desktop.enable = false;
-    lan.enable = false;
-    cachix.enable = false;
+    desktop.keybase.enable = false;
+    cachix.enable = true;
+    nodeExporter.enable = false;
 
     os = {
       boot = {
         uefi = true;
         device = "/dev/sda";
       };
-      externalInterface = "eno1";
+
+      networkmanager.enable = true;
+      externalInterface = "eno2";
+
+      docker.enable = false;
+
+      adminAuthorizedKeys = [
+      ];
     };
 
-    # hpc = {
-    #   prometheusServer = true;
-    #   # prometheusExporter = true;
-    #   monitoring.server.enable = true;
-    #   monitoring.server.scrapeHosts = [
-    #     "vortex"
-    #   ];
-
-    #   mungeKey = ./munge.key;
-
-    #   firewall = {
-    #     allowedTCPPorts = [ 80 443 6817 6818 6819 ];
-    #     allowedUDPPorts = [];
-    #   };
-
-    #   slurm = {
-    #     server = true;
-    #     controlMachine = "vortex";
-    #     nodeName = [
-    #       "c0-[1-8] CPUs=64 RealMemory=196000 TmpDisk=250000 State=UNKNOWN"
-    #       "yoneda  CPUs=12 Sockets=1 CoresPerSocket=6 ThreadsPerCore=2 RealMemory=8000 TmpDisk=1000 State=UNKNOWN"
-    #     ];
-    #     partitionName = [
-    #       "batch Nodes=c0-[1-8],yoneda Default=YES MaxTime=INFINITE State=UP"
-    #     ];
-    #   };
-    # };
-
+    lan.enable = false;
   };
-
-  services.dnsmasq.enable = true;
-  services.dnsmasq.extraConfig = ''
-    address=/.cluster.local/10.101.0.1
-  '';
-
-  users.extraUsers.root.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKiAS30ZO+wgfAqDE9Y7VhRunn2QszPHA5voUwo+fGOf jonas-3"
-    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDULdlLC8ZLu9qBZUYsjhpr6kv5RH4yPkekXQdD7prkqapyoptUkO1nOTDwy7ZsKDxmp9Zc6OtdhgoJbowhGW3VIZPmooWO8twcaYDpkxEBLUehY/n8SlAwBtiHJ4mTLLcynJMVrjmTQLF3FeWVof0Aqy6UtZceFpLp1eNkiHTCM3anwtb9+gfr91dX1YsAOqxqv7ooRDu5rCRUvOi4OvRowepyuBcCjeWpTkJHkC9WGxuESvDV3CySWkGC2fF2LHkAu6SFsFE39UA5ZHo0b1TK+AFqRFiBAb7ULmtuno1yxhpBxbozf8+Yyc7yLfMNCyBpL1ci7WnjKkghQv7yM1xN2XMJLpF56v0slSKMoAs7ThoIlmkRm/6o3NCChgu0pkpNg/YP6A3HfYiEDgChvA6rAHX6+to50L9xF3ajqk4BUzWd/sCk7Q5Op2lzj31L53Ryg8vMP8hjDjYcgEcCCsGOcjUVgcsmfC9LupwRIEz3aF14AWg66+3zAxVho8ozjes= jonas.juselius@juselius.io"
-  ];
-
-  # services.samba = {
-  #   enable = true;
-  #   shares = {
-  #     public = {
-  #       path = "/srv/public";
-  #       "read only" = true;
-  #       browseable = "yes";
-  #       "guest ok" = "yes";
-  #       comment = "Public samba share.";
-  #     };
-  #   };
-  # };
 }
 
