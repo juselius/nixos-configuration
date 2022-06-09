905144611eb701593bcaf3e0d303095e721bbf4b
Jonas Juselius <jonas.juselius@itpartner.no>
2022-06-09 10:14:54 +0200

    fix: fix locale for number formatting

--- configuration.nix
+++ configuration.nix
@@ -49,9 +49,16 @@ in
     keyMap = "us";
   };
   i18n = {
-    defaultLocale = "en_DK.UTF-8";
+    defaultLocale = "en_US.UTF-8";
     extraLocaleSettings = {
-      LC_TIME = "en_DK.UTF-8";
+      LC_CTYPE="en_DK.UTF-8";
+      LC_TIME="en_DK.UTF-8";
+      LC_PAPER="en_DK.UTF-8";
+      LC_NAME="en_DK.UTF-8";
+      LC_ADDRESS="en_DK.UTF-8";
+      LC_TELEPHONE="en_DK.UTF-8";
+      LC_MEASUREMENT="en_DK.UTF-8";
+      LC_IDENTIFICATION="en_DK.UTF-8";
     };
   };
 
